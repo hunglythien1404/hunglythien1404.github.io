b09691c058c63fe00decac93f6af6972302da8d9
hunglythien <hung.lythien@gmail.com>
2018-10-31 22:26:14 +0700

    Updates

---
+++ precache-manifest.4522ebde2935bacab46f8209d1a0bb44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "612173fe8f0db271f7db",
+    "url": "/static/js/main.612173fe.chunk.js"
+  },
+  {
+    "revision": "cc4330fbb1f4b7366438",
+    "url": "/static/js/1.cc4330fb.chunk.js"
+  },
+  {
+    "revision": "612173fe8f0db271f7db",
+    "url": "/static/css/main.624c9b36.chunk.css"
+  },
+  {
+    "revision": "e0f32cd4eb161724942501459893ec72",
+    "url": "/index.html"
+  }
+];
